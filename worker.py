b8122fe2757b836b25793a78015368283bcdcc69
anonnimo2 <manecomonteirofino@gmail.com>
2025-08-11 19:58:36 +0100

    Adicionar JobLog

--- worker.py
+++ worker.py
@@ -22,12 +22,11 @@ def loop_worker():
     with app.app_context():
         while True:
             agora = datetime.now(LUANDA_TZ)
-            # Executa a cada 4 minutos
-            if agora.minute % 4 == 0 and not ja_executou_hoje(f"distribuir_rendimentos_{agora.hour}_{agora.minute}"):
+            if agora.hour == 0 and agora.minute == 0 and not ja_executou_hoje("distribuir_rendimentos"):
                 print(f"[{agora}] 🚀 Executando distribuição de rendimentos...")
                 try:
                     distribuir_rendimentos()
-                    registrar_execucao(f"distribuir_rendimentos_{agora.hour}_{agora.minute}")
+                    registrar_execucao("distribuir_rendimentos")
                     print(f"[{agora}] ✅ Rendimento distribuído com sucesso.")
                 except Exception as e:
                     print(f"[{agora}] ❌ Erro ao distribuir rendimentos: {e}")
@@ -35,5 +34,5 @@ def loop_worker():
             time.sleep(20)
 
 if __name__ == "__main__":
-    print("🕒 Worker iniciado. Testando execução a cada 4 minutos...")
+    print("📅 Worker iniciado. Aguardando meia-noite para execução...")
     loop_worker()
